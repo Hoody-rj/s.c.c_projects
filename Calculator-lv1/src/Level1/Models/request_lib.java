b27dca4cf181bf16f65cea4a5fa879cbef4c8c21
Shaun.the.sheep <robin.Jeong817@gmail.com>
2025-07-09 14:29:05 +0900

    Delete Calculator-lv1 directory

--- Calculator-lv1/src/Level1/Models/request_lib.java
+++
@@ -1,8 +0,0 @@
-package Level1.Models;
-
-public class request_lib {
-    public String strException = "";
-    public boolean resultBool = true;
-    public double resultInt;
-}
-
--- Calculator-lv1/src/Level2to3/ControllerConfig.java
+++
@@ -1,7 +0,0 @@
-package Level2to3;
-
-import Level2to3.Models.calController;
-
-public class ControllerConfig {
-    public static calController conData = new calController();
-}
